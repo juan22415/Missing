eb1d0b2fc8ef38f447fd2e23d9d798e2bc735569
juan camilo rios <juan22415@hotmail.com>
2017-05-01 14:15:19 -0500

    Animaciones y linterna

--- Assets/Scripts/Emily_Rotation.cs
+++ Assets/Scripts/Emily_Rotation.cs
@@ -9,46 +9,51 @@ public class Emily_Rotation : MonoBehaviour {
     private Rigidbody emily_rigidbody;
     [SerializeField]
     private GameObject flashlight;
+    public Animator anim;
 
 
 
     // Use this for initialization
     void Awake ()
     {
-        emily_rigidbody = GetComponent<Rigidbody>();	
+        emily_rigidbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-       // Debug.Log(flashlight.transform.localEulerAngles);
-
-        if (emily_rigidbody.velocity.x > 0 && !facingright)
-        {
-            flip();
-        }
-        else if (emily_rigidbody.velocity.x < 0 && facingright)
-        {
-            flip();
-        }
-
-        else if (flashlight.transform.localEulerAngles.y > 0 && flashlight.transform.localEulerAngles.y < 180)
-        {
-            Debug.Log("mirando a la derecha");
-        }
-
-        else if (flashlight.transform.localEulerAngles.y > 180 && flashlight.transform.localEulerAngles.y < 360)
-        {
-            Debug.Log("mirando a la izquierda");
-        }
-    }
-
-    void flip()
+	void Update ()
     {
 
-        facingright = !facingright;
-        Vector3 myscale = transform.localScale;
-        myscale.x *= -1;
-        transform.localScale = myscale;
+        //Debug.Log(flashlight.transform.localEulerAngles.y);
+
+        if (flashlight.transform.localEulerAngles.y > 345 || flashlight.transform.localEulerAngles.y < 15)
+        {
+            anim.SetInteger("direction", 4);
+        }
+
+        else if (flashlight.transform.localEulerAngles.y > 15 && flashlight.transform.localEulerAngles.y < 60)
+        {
+            anim.SetInteger("direction", 3);
+        }
+
+        else if (flashlight.transform.localEulerAngles.y > 60 && flashlight.transform.localEulerAngles.y < 165)
+        {
+            anim.SetInteger("direction", 2);
+        }
+
+        else if (flashlight.transform.localEulerAngles.y > 165 && flashlight.transform.localEulerAngles.y < 195)
+        {
+            anim.SetInteger("direction", 1);
+        }
+
+        else if (flashlight.transform.localEulerAngles.y > 195 && flashlight.transform.localEulerAngles.y < 300)
+        {
+            anim.SetInteger("direction", 6);
+        }
+
+        else if (flashlight.transform.localEulerAngles.y > 300 && flashlight.transform.localEulerAngles.y < 345)
+        {
+            anim.SetInteger("direction", 5);
+        }
     }
 }
---
+++ Assets/spritechange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spritechange : MonoBehaviour {
+
+    public Sprite[] spritearray;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.localEulerAngles.y > 345 || transform.localEulerAngles.y < 15)
+        {
+            GetComponent<SpriteRenderer>().sprite = spritearray[4];
+        }
+
+        if (transform.localEulerAngles.y > 15 && transform.localEulerAngles.y < 60)
+        {
+            GetComponent<SpriteRenderer>().sprite = spritearray[3];
+        }
+        if (transform.localEulerAngles.y > 60 && transform.localEulerAngles.y < 165)
+        {
+            GetComponent<SpriteRenderer>().sprite = spritearray[2];
+        }
+        if (transform.localEulerAngles.y > 165 && transform.localEulerAngles.y < 195)
+        {
+            GetComponent<SpriteRenderer>().sprite = spritearray[1];
+        }
+        if (transform.localEulerAngles.y > 195 && transform.localEulerAngles.y < 300)
+        {
+            GetComponent<SpriteRenderer>().sprite = spritearray[6];
+        }
+        if (transform.localEulerAngles.y > 300 && transform.localEulerAngles.y < 345)
+        {
+            GetComponent<SpriteRenderer>().sprite = spritearray[5];
+        }
+
+    }
+}
