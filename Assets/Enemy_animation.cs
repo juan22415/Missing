eb22284b784467c81a4b1d4cd38f6debc2cc45fd
juan camilo rios <juan22415@hotmail.com>
2017-05-03 12:39:55 -0500

    fiiiiire

---
+++ Assets/Enemy_animation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_animation : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+    private Animator animator;
+    private Transform transform;
+    Vector3 theScale;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
+        Vector3 theScale = transform.localScale;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log(rigidbody.velocity);
+
+        if (rigidbody.velocity.x>0 && rigidbody.velocity.y > 0)
+        {
+            animator.SetBool("movingfront", false);
+            theScale.x = 1;
+            transform.localScale = theScale;
+        }
+
+        else if (rigidbody.velocity.x > 0 && rigidbody.velocity.y < 0)
+        {
+            animator.SetBool("movingfront", true);
+            theScale.x = 1;
+            transform.localScale = theScale;
+        }
+
+        else if (rigidbody.velocity.x < 0 && rigidbody.velocity.y > 0)
+        {
+            animator.SetBool("movingfront", false);
+            theScale.x = -1;
+            transform.localScale = theScale;
+        }
+
+        else if (rigidbody.velocity.x < 0 && rigidbody.velocity.y < 0)
+        {
+            animator.SetBool("movingfront", true);
+            theScale.x = -1;
+            transform.localScale = theScale;
+        }
+
+    }
+}
